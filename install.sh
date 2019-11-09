eb551e0cf2f860c421a720332dc8a9b8fb02d1df
Sathwik Matsa <sathwikmatsa@gmail.com>
2019-11-09 18:20:17 +0530

    attempt to fix ad hoc install script [2]

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cargo install diesel_cli --no-default-features --features sqlite
-
-read -p "enter database name: " dbname
-echo "DATABASE_URL=$dbname" > .env
-
-diesel migration run
-
-cargo build
--- termux_install.sh
+++ termux_install.sh
@@ -1,28 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 pkg install rust
 pkg install sqlite
 
 cargo install diesel_cli --no-default-features --features sqlite
 
-PATH=/data/data/com.termux/files/home/.cargo/bin:$PATH
-
 CARGO_HOME="$HOME/.cargo"
 CARGO_BIN="$CARGO_HOME/bin"
 INSTALL_DIR="$CARGO_BIN/accountant"
 
+mkdir -p $INSTALL_DIR
+
 echo "DATABASE_URL=$INSTALL_DIR/.acc.db" > .env
 ENV_VAR="DATABASE_URL=$INSTALL_DIR/.acc.db"
+
+export PATH=/data/data/com.termux/files/home/.cargo/bin:$PATH
 
 diesel setup
 
 cargo build --release
 
-mkdir -p $INSTALL_DIR
 cp target/release/accountant $INSTALL_DIR
-cp *.db $INSTALL_DIR
 cp .env $INSTALL_DIR
 
-echo "alias acc=\"$ENV_VAR $INSTALL_DIR/accountant\"" >> ~/.bashrc
+if ! cat ~/.bashrc | grep -q "alias acc"; then
+    echo "alias acc=\"$ENV_VAR $INSTALL_DIR/accountant\"" >> ~/.bashrc
+fi
+if ! cat ~/.bashrc | grep -q "cargo/bin"; then
+    echo "export PATH=/data/data/com.termux/files/home/.cargo/bin:$PATH" >> ~/.bashrc
+fi
+
 source ~/.bashrc
 
 echo "Successfully installed 'accountant' cli"
